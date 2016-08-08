d13df964c8a5fe6b5b329911ec0778ba944f3d72
lujanfernaud <lujanfernaud@gmail.com>
2016-08-08 20:15:23 +0100

    Add before filter

--- main.rb
+++ main.rb
@@ -17,6 +17,16 @@ helpers do
   end
 end
 
+# Locks access to paths in array unless the user is logged in.
+['/post', '/post/:id/edit', '/post/:id/delete'].each do |path|
+  before path do
+    if !session[:loggedin]
+      redirect '/'
+    end
+  end
+end
+
+# Create a new instance of the Blog class and save it in the 'db' variable.
 db = Blog.new
 
 # Index (quite obvious).
@@ -45,44 +55,27 @@ end
 
 # Show the form to create a new post.
 get '/post' do
-  if !session[:loggedin]
-    redirect '/'
-  end
-
   haml :blog_form
 end
 
 # Store the information of the new post into the database
 # and redirect to the post.
 post '/post' do
-  if !session[:loggedin]
-    redirect '/'
-  end
-
   id = db.new_post(params[:title], params[:body])
   redirect "/post/#{id}"
 end
 
 # Edit a post.
 get '/post/:id/edit' do
-  if !session[:loggedin]
-    redirect '/'
-  end
-
   @post = db.post(params[:id])
   @title = @post[:title]
   @body = @post[:body]
-
   haml :blog_form_edit
 end
 
 # Send the new information of the edited post to the database
 # and redirect to the post.
 post '/post/:id/edit' do
-  if !session[:loggedin]
-    redirect '/'
-  end
-
   db.update_post(params[:id], params[:title], params[:body])
   redirect "/post/#{params[:id]}"
 end
@@ -90,20 +83,12 @@ end
 # Shows a preview of the post to delete, 
 # and asks for confirmation before deleting it.
 get '/post/:id/delete' do
-  if !session[:loggedin]
-    redirect '/'
-  end
-  
   @post = db.post(params[:id])
   haml :blog_post_delete
 end
 
 # Deletes the post from the database and redirects to the index.
-post '/post/:id/delete' do
-  if !session[:loggedin]
-    redirect '/'
-  end
-  
+post '/post/:id/delete' do 
   db.delete_post(params[:id])
   redirect '/'
 end
